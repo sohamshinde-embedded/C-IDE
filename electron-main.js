0c4e5eb7675999b12614aea0ca6f9b3467811846
sohamshinde-embedded <sohamshinde608@gmail.com>
2026-06-08 12:31:16 +0530

    add silent auto-updator

--- electron-main.js
+++ electron-main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const path = require('path');
 
 // Require the express server so it starts running
@@ -9,8 +10,8 @@ function createWindow () {
     width: 1280,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      nodeIntegration: false,
+      contextIsolation: true
     },
     autoHideMenuBar: true,
     title: "CMentor"
@@ -22,6 +23,23 @@ function createWindow () {
 
 app.whenReady().then(() => {
   createWindow();
+
+  // Check for updates automatically in the background
+  autoUpdater.checkForUpdatesAndNotify();
+
+  // When an update is downloaded, notify the user
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Update Ready',
+      message: 'A new version of CMentor has been downloaded. Restart the application to apply the updates.',
+      buttons: ['Restart', 'Later']
+    }).then((result) => {
+      if (result.response === 0) {
+        autoUpdater.quitAndInstall();
+      }
+    });
+  });
 
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
